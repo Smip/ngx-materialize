073aa72c963a6654eb18bb273e2283c90e062cd7
stein <stein.vanmassenhove@gmail.com>
2023-01-07 10:42:16 +0100

    update public-api

--- projects/smip/ngx-materialize/src/public_api.ts
+++ projects/smip/ngx-materialize/src/public_api.ts
@@ -1,7 +1,26 @@
 /*
  * Public API Surface of ngx-materialize
  */
-export * from './lib/types';
-export * from './lib/ngx-materialize.module';
-export * from './lib/functions';
+export {AutocompleteInstance, AutocompleteOptions,DropdownInstance, DropdownOptions, FormSelectInstance, FormSelectOptions, Toast } from './lib/types';
+export {NgxMaterialize} from './lib/ngx-materialize.module';
+export {AutocompleteDirective} from './lib/directives/autocomplete.directive';
+export {CarouselDirective} from './lib/directives/carousel.directive';
+export {ChipsDirective} from './lib/directives/chips.directive';
+export {CollapsibleDirective} from './lib/directives/collapsible.directive';
+export {DatepickerDirective} from './lib/directives/datepicker.directive';
+export {DropdownDirective} from './lib/directives/dropdown.directive';
+export {FormSelectDirective} from './lib/directives/form-select.directive';
+export {MaterialboxDirective} from './lib/directives/materialbox.directive';
+export {ModalDirective} from './lib/directives/modal.directive';
+export {ParallaxDirective} from './lib/directives/parallax.directive';
+export {PushpinDirective} from './lib/directives/pushpin.directive';
+export {ScrollSpyDirective} from './lib/directives/scroll-spy.directive';
+export {SidenavDirective} from './lib/directives/sidenav.directive';
+export {SliderDirective} from './lib/directives/slider.directive';
+export {TabsDirective} from './lib/directives/tabs.directive';
+export {TapTargetDirective} from './lib/directives/tap-target.directive';
+export {TimepickerDirective} from './lib/directives/timepicker.directive';
+export {TooltipDirective} from './lib/directives/tooltip.directive';
+
+export {textareaAutoResize, toast, updateTextFields} from './lib/functions';
 
