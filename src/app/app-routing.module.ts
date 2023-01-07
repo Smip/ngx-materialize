2eff8142dd329c539b08e57c8f43524f5bcff59a
stein <stein.vanmassenhove@gmail.com>
2023-01-07 10:23:37 +0100

    angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,8 +15,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    anchorScrolling: 'enabled',
-    relativeLinkResolution: 'legacy'
+    anchorScrolling: 'enabled'
 })
   ],
   exports: [RouterModule]
